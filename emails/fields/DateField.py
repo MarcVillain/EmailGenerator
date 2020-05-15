4b5eed48465241ce209544027bf0c2bbd584ca54
Marc Villain <marc.villain@epita.fr>
2020-05-15 10:30:26 +0200

    feat: randomize date field

--- emails/fields/DateField.py
+++ emails/fields/DateField.py
@@ -1,13 +1,16 @@
 from datetime import datetime
 
 from emails.fields import Field
+from helpers.DatesHelper import DatesHelper
 
 
 class DateField(Field):
     def __init__(self, email):
         super().__init__(email)
 
-        self.date = datetime.now()
+        date_from = datetime.strptime("1/1/2020 01:00 AM", "%d/%m/%Y %I:%M %p")
+        date_to = datetime.strptime("31/12/2020 11:00 PM", "%d/%m/%Y %I:%M %p")
+        self.date = DatesHelper.random_between(date_from, date_to).astimezone()
 
     def __str__(self):
-        return str(self.date)
+        return str(self.date.strftime("%a, %d %b %Y %H:%M:%S %z"))
---
+++ helpers/DatesHelper.py
@@ -0,0 +1,11 @@
+from datetime import timedelta
+import random
+
+
+class DatesHelper:
+    @staticmethod
+    def random_between(start, end):
+        delta = end - start
+        int_delta = (delta.days * 24 * 60 * 60) + delta.seconds
+        random_second = random.randrange(int_delta)
+        return start + timedelta(seconds=random_second)
